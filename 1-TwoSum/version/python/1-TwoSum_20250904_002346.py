5aaf5941f2aed7a79ebb0048de6edd6aecc76915
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2025-09-03 21:23:46 -0700

    Create 1-TwoSum

---
+++ 1-TwoSum/version/python/1-TwoSum_20250904_002346.py
@@ -0,0 +1,16 @@
+# Last updated: 9/4/2025, 12:23:46 AM
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in seen:
+                return [seen[diff], i]
+            else:
+                seen[nums[i]] = i
+            
