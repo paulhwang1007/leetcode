7293b5edff7aa641bd33b99c19e65d2fe834dec0
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2025-09-07 10:18:58 -0700

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/version/python/217-ContainsDuplicate_20250907_131858.py
@@ -0,0 +1,8 @@
+# Last updated: 9/7/2025, 1:18:58 PM
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums)) != len(nums)
