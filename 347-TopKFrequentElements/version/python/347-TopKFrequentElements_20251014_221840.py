f169e057b0aa63e9dd6701d430dee661953a8f4b
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2025-10-14 22:18:41 -0400

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/version/python/347-TopKFrequentElements_20251014_221840.py
@@ -0,0 +1,23 @@
+# Last updated: 10/14/2025, 10:18:40 PM
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for token in tokens:
+
+            if token == "+":
+                num1, num2 = int(stack.pop()), int(stack.pop())
+                stack.append(num1 + num2)
+            elif token == "-":
+                num1, num2 = int(stack.pop()), int(stack.pop())
+                stack.append(num2 - num1)
+            elif token == "*":
+                num1, num2 = int(stack.pop()), int(stack.pop())
+                stack.append(num1 * num2)
+            elif token == "/":
+                num1, num2 = int(stack.pop()), int(stack.pop())
+                stack.append(num2 / num1)
+            else:
+                stack.append(int(token))
+
+        return int(stack[-1])
