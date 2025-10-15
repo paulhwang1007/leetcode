4c8ccca620a2359b73072ccd1e2b575fe6b0c812
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2025-10-15 16:06:51 -0400

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/version/python/347-TopKFrequentElements_20251015_160651.py
@@ -0,0 +1,11 @@
+# Last updated: 10/15/2025, 4:06:51 PM
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p,s] for p, s in zip(position, speed)]
+        stack = []
+
+        for p, s in sorted(pair)[::-1]:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
