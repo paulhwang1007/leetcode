dbac4f845082db54b0a33eace19da37b3467ff5a
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2025-12-07 00:20:30 -0500

    Create 15-3Sum

---
+++ 15-3Sum/version/python/15-3Sum_20251207_002030.py
@@ -0,0 +1,17 @@
+# Last updated: 12/7/2025, 12:20:30 AM
+1class Solution:
+2    def simplifyPath(self, path: str) -> str:
+3        stack = []
+4        string = ""
+5
+6        for char in path + "/":
+7            if char == "/":
+8                if string == "..":
+9                    if stack:
+10                        stack.pop()
+11                elif string != "" and string != ".":
+12                    stack.append(string)
+13                string = ""
+14            else:
+15                string += char
+16        return "/" + "/".join(stack)
