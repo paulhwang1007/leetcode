94e5faaecf7af2ee2fa14906a8766fe9c3a7e0ec
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2025-12-07 15:31:02 -0500

    Create 15-3Sum

---
+++ 15-3Sum/version/python/15-3Sum_20251207_153059.py
@@ -0,0 +1,20 @@
+# Last updated: 12/7/2025, 3:30:59 PM
+1class Solution:
+2    def decodeString(self, s: str) -> str:
+3        stack = []
+4
+5        for i in range(len(s)):
+6            if s[i] != "]":
+7                stack.append(s[i])
+8            else:
+9                substr = ""
+10                while stack[-1] != "[":
+11                    substr = stack.pop() + substr
+12                stack.pop()
+13
+14                k = ""
+15                while stack and stack[-1].isdigit():
+16                    k = stack.pop() + k
+17                
+18                stack.append(int(k) * substr)
+19        return ''.join(stack)
