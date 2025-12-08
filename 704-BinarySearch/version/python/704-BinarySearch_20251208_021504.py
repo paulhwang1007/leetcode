ad21df8b92bde7fed3898d274372d3bbaf2db53f
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2025-12-08 02:15:04 -0500

    Create 704-BinarySearch

---
+++ 704-BinarySearch/version/python/704-BinarySearch_20251208_021504.py
@@ -0,0 +1,18 @@
+# Last updated: 12/8/2025, 2:15:04 AM
+1class Solution:
+2    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+3        for i in range(len(matrix)):
+4            if matrix[i][-1] < target:
+5                continue
+6            left, right = 0, len(matrix[i]) - 1
+7            while left <= right:
+8                mid = (left + right) // 2
+9                if matrix[i][mid] > target:
+10                    right = mid - 1
+11                elif matrix[i][mid] < target:
+12                    left = mid + 1
+13                else:
+14                    return target in matrix[i]
+15        return False
+16            
+17            
