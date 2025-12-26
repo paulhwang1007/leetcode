6a9e452e012882e777565dfec0f86a5ed7ca481a
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2025-12-25 22:48:05 -0800

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/version/python/242-ValidAnagram_20251225_224805.py
@@ -0,0 +1,12 @@
+# Last updated: 12/25/2025, 10:48:05 PM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        pairs = {}
+4
+5        for i in range(len(nums)):
+6            diff = target - nums[i]
+7
+8            if diff in pairs:
+9                return [i, pairs[diff]]
+10            else:
+11                pairs[nums[i]] = i
