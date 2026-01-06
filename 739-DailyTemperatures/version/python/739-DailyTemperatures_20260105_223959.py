3eab070de30c4fba5d649286465f9e9a4fc4adf7
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2026-01-05 22:39:59 -0800

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/version/python/739-DailyTemperatures_20260105_223959.py
@@ -0,0 +1,11 @@
+# Last updated: 1/5/2026, 10:39:59 PM
+1class Solution:
+2    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+3        cars = [[p, s] for p, s in zip(position, speed)]
+4        stack = []
+5
+6        for p,s in sorted(cars)[::-1]:
+7            stack.append((target - p) / s)
+8            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+9                stack.pop()
+10        return len(stack)
