2556c59ad6b5d7ba097899069c8ee80e42af7486
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2026-01-29 09:05:02 -0800

    Create 206-ReverseLinkedList

---
+++ 206-ReverseLinkedList/version/python/206-ReverseLinkedList_20260129_120501.py
@@ -0,0 +1,20 @@
+# Last updated: 1/29/2026, 12:05:01 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+9        if not root:
+10            return None
+11        
+12        temp = root.left
+13        root.left = root.right
+14        root.right = temp
+15
+16        self.invertTree(root.left)
+17        self.invertTree(root.right)
+18
+19        return root
