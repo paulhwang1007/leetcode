5997ec7a577cc18e653a81418fc7ad0cb13a8a6f
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2026-07-26 15:53:01 -0700

    Create 817-DesignHashmap

---
+++ 817-DesignHashmap/817-DesignHashmap.py
@@ -0,0 +1,25 @@
+# Last updated: 7/26/2026, 3:53:00 PM
+class MyHashMap:
+
+    def __init__(self):
+        self.map = {}
+
+    def put(self, key: int, value: int) -> None:
+            self.map[key] = value
+
+    def get(self, key: int) -> int:
+        if key in self.map:
+            return self.map[key]
+        else:
+            return -1
+
+    def remove(self, key: int) -> None:
+        if key in self.map:
+            self.map.pop(key)   
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
