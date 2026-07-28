083b0b88de9f01e78b52346fe86b5e4a4b732a24
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2026-07-27 21:39:59 -0700

    File updated at 7/27/2026, 9:39:59 PM

--- 141-LinkedListCycle/141-LinkedListCycle.py
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -1,18 +1,26 @@
-# Last updated: 7/27/2026, 9:32:16 PM
+# Last updated: 7/27/2026, 9:39:59 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
 4#         self.val = val
 5#         self.next = next
 6class Solution:
-7    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-8        prev = None
-9        curr = head
+7    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+8        dummy = ListNode()
+9        tail = dummy
 10
-11        while curr:
-12            next_node = curr.next
-13            curr.next = prev
-14            prev = curr
-15            curr = next_node
-16        head = prev
-17        return head
+11        while list1 and list2:
+12            if list1.val < list2.val:
+13                tail.next = list1
+14                list1 = list1.next
+15            else:
+16                tail.next = list2
+17                list2 = list2.next
+18            tail = tail.next
+19        
+20        if list1:
+21            tail.next = list1
+22        else:
+23            tail.next = list2
+24
+25        return dummy.next
