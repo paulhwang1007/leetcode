e05170b7f81b0dfc74c746f21dce75323cfd458e
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2026-07-28 23:38:16 -0700

    File updated at 7/28/2026, 11:38:16 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,23 +1,25 @@
-# Last updated: 7/26/2026, 3:53:11 PM
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
-        buckets = [[] for i in range(len(nums) + 1)]
-        res = []
-
-        # count frequencies
-        for num in nums:
-            if num not in freq:
-                freq[num] = 0
-            freq[num] += 1
-        
-        # append into buckets
-        for num, freq in freq.items():
-            buckets[freq].append(num)
-
-        # iterate through buckets
-        for bucket in range(len(buckets) - 1, 0, -1):
-            for num in buckets[bucket]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+# Last updated: 7/28/2026, 11:38:16 PM
+1class Solution:
+2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+3        freq = {}
+4        buckets = [[] for i in range(len(nums) + 1)]
+5        topK = []
+6
+7        # get frequencies
+8        # { num: freq }
+9        for num in nums:
+10            if num not in freq:
+11                freq[num] = 0
+12            freq[num] += 1
+13        
+14        # append numbers to bucket
+15        for num, count in freq.items():
+16            buckets[count].append(num)
+17        
+18        # return top k
+19        for bucket in range(len(buckets) - 1, 0, -1):
+20            for num in buckets[bucket]:
+21                topK.append(num)
+22
+23                if len(topK) == k:
+24                    return topK
