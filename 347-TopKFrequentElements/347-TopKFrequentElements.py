e892a56f023ffdb61aca001469ebbd5e862aa075
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2026-07-29 23:27:40 -0700

    File updated at 7/29/2026, 11:27:40 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,4 +1,4 @@
-# Last updated: 7/29/2026, 12:05:04 AM
+# Last updated: 7/29/2026, 11:27:40 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -9,27 +9,31 @@
 8        """
 9        Do not return anything, modify head in-place instead.
 10        """
-11        # find the midpoint of the list
-12        slow, fast = head, head.next
-13
+11        slow, fast = head, head.next
+12
+13        # 1. split the list in half
 14        while fast and fast.next:
 15            slow = slow.next
 16            fast = fast.next.next
 17
-18        # reverse the second half
+18        # 2. reverse the second half
 19        second = slow.next
-20        prev = None
-21        slow.next = None
-22        while second:
-23            next_node = second.next
-24            second.next = prev
-25            prev = second
-26            second = next_node
-27
-28        # merge the lists
-29        first, second = head, prev
-30        while second:
-31            next_1, next_2 = first.next, second.next
-32            first.next = second
-33            second.next = next_1
-34            first, second = next_1, next_2
+20        prev = slow.next = None
+21        while second:
+22            next_node = second.next
+23            second.next = prev
+24            prev = second
+25            second = next_node
+26        
+27        # 3. reorder the nodes
+28        # Keep track of:
+29        #       - head: points to the head
+30        #       - prev: points to the last node since second is None rn
+31        first, second = head, prev
+32        while second:
+33            first_next, second_next = first.next, second.next
+34            first.next = second
+35            second.next = first_next
+36            first, second = first_next, second_next
+37        
+38        
