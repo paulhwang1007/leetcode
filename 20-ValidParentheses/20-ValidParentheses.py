f764dec0644a2aa3ff7cbc6a58343c54afee2d0f
Paul Hwang <168374431+paulhwang1007@users.noreply.github.com>
2026-08-01 13:26:37 -0700

    File updated at 8/1/2026, 1:26:37 PM

--- 20-ValidParentheses/20-ValidParentheses.py
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -1,16 +1,13 @@
-# Last updated: 7/26/2026, 3:53:39 PM
-class Solution:
-    def isValid(self, s: str) -> bool:
-        paren = {")":"(", "}":"{", "]":"["}
-        stack = []
-
-        for char in s:
-            if char in paren:
-                if not stack or paren[char] != stack[-1]:
-                    return False
-                else: 
-                    stack.pop()
-            else:
-                stack.append(char)
-        return not stack
-
+# Last updated: 8/1/2026, 1:26:37 PM
+1class Solution:
+2    def isValid(self, s: str) -> bool:
+3        brackets = { "(": ")", "{": "}", "[": "]" }
+4        stack = []
+5
+6        for bracket in s:
+7            if bracket in brackets:
+8                stack.append(bracket)
+9            elif len(stack) == 0 or brackets[stack.pop()] != bracket:
+10                return False
+11        return len(stack) == 0
+12
